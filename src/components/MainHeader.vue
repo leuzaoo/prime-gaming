87e0e2e59df7402a39dcc90ef10084957fe3d1f1
leuzaoo <leuzaoocontato@gmail.com>
2022-10-03 10:50:21 -0300

    Início do Header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="flex">
-    <p>treste</p>
-    <p>treste</p>
-    <p>treste</p>
+  <div class="flex bg-[#0B0418] text-white h-14 items-center pl-6">
+    <ul>
+      <li>
+        <p class="font-bold">prime <span class="font-normal">gaming</span></p>
+      </li>
+    </ul>
   </div>
 </template>
