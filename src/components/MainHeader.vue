f87f1fbc83f77e4a2f05426dd287e85c0021ed24
leuzaoo <leuzaoocontato@gmail.com>
2022-10-05 10:08:07 -0300

    Atualizado o MainHeader

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class="flex bg-[#0B0418] text-white h-14 items-center pl-6">
-    <ul>
+  <div class="flex bg-[#0B0418] text-white h-14 justify-center">
+    <ul class="flex gap-2 items-center">
       <li>
         <p class="font-bold">prime <span class="font-normal">gaming</span></p>
+      </li>
+      <li>
+        <p class="bg-[#5FB7FF] px-2 py-1 rounded text-sm text-black">
+          Experimente Amazon Prime
+        </p>
+      </li>
+      <li class="flex items-center ml-3">
+        <remix-icon icon="user" :size="16" />
+        <remix-icon icon="arrow-down-s" :size="20" />
       </li>
     </ul>
   </div>
 </template>
+
+<script lang="ts" setup>
+import RemixIcon from "./RemixIcon.vue";
+</script>
