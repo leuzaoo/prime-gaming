7ab716748f7efd1629a4e11bc3269393b4eac21a
leuzaoo <leuzaoocontato@gmail.com>
2022-10-06 23:08:37 -0300

    Atualizado a aplicação

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <main-header />
+
+  <image-cover src="https://i.redd.it/e2aiwmfjv0g51.png"> </image-cover>
+  <h2 class="-mt-10 relative text-white font-bold justify-center flex">Pokemon GO</h2>
 </template>
 
 <script lang="ts" setup>
-  import MainHeader from './components/MainHeader.vue';
+import MainHeader from "./components/MainHeader.vue";
+import ImageCover from "./components/ImageCover.vue";
 </script>
