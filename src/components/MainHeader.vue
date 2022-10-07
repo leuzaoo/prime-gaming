2f83c0ed10478bf9b955fb31dbae633fd8756158
leuzaoo <leuzaoocontato@gmail.com>
2022-10-06 23:08:30 -0300

    Atualizado o MainHeader

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex bg-[#0B0418] text-white h-14 justify-center">
+  <div class="flex bg-[#0B0418] text-white h-14 justify-around shadow-2xl z-10 relative">
     <ul class="flex gap-2 items-center">
       <li>
         <p class="font-bold">prime <span class="font-normal">gaming</span></p>
