98d0e0c201c7ebb14071b406827eea8b94f8f1d6
leuzaoo <leuzaoocontato@gmail.com>
2022-10-26 21:33:49 -0300

    Atualizado para versão desktop

--- src/components/ImageCover.vue
+++ src/components/ImageCover.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="relative max-h-40 md:max-h-80 xl:max-h-[600px] xl:max-h-96 overflow-hidden"
-  >
+  <div class="relative max-h-40 md:max-h-80 xl:max-h-[600px] overflow-hidden">
     <img class="object-cover w-full h-full" :src="src" :alt="alt" />
     <div
       class="absolute top-0 left-0 bottom-0 right-0 bg-gradient-to-b from-transparent to-black opacity-90"
