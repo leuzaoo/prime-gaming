1fd1a187c07f8ddf644fbe48d5ca2f50be292b78
leuzaoo <leuzaoocontato@gmail.com>
2022-11-08 15:55:04 -0300

    Apagado e instalado novamente Package.json

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
+
 import "./style.css";
 import "./CarouselStyle.css";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 import App from "./App.vue";
 
 createApp(App).mount("#app");
