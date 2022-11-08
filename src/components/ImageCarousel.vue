34be01b694adfd7cacd19d259f50770e9491295e
leuzaoo <leuzaoocontato@gmail.com>
2022-11-08 14:39:05 -0300

    Atualizado "Carousel Component"

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -1,15 +1,29 @@
 <template>
   <section>
-    <div id="container">
-      <div id="content">
-        <div id="card">
-          <div id="card-content">
-            <div id="image">
+    <div class="container">
+      <div class="content">
+        <div class="card">
+          <div class="card-content">
+            <div class="image">
               <img
-                src="https://cdn.oneesports.gg/cdn-data/2022/09/LeagueofLegends_Worlds2022_Logo_Poster.jpg"
-                alt=""
+                class="img-game"
+                src="https://cdn.cloudflare.steamstatic.com/steam/apps/1172470/capsule_616x353.jpg?t=1664321959"
               />
             </div>
+
+            <div class="media-icons gap-5">
+              <i><remix-icon icon="world" :size="20"></remix-icon></i>
+              <i><remix-icon icon="user" :size="20"></remix-icon></i>
+              <i><remix-icon icon="lupa" :size="20"></remix-icon></i>
+            </div>
+
+            <div class="name-profession">
+              <span class="name">Someone Name</span>
+              <span class="profession">Web Developer</span>
+            </div>
+
+
+            
           </div>
         </div>
       </div>
@@ -17,4 +31,6 @@
   </section>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import RemixIcon from "./RemixIcon.vue";
+</script>
