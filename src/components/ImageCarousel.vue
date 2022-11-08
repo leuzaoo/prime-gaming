a16f670588d067589c98d633c9a3f063f6330b34
leuzaoo <leuzaoocontato@gmail.com>
2022-11-08 01:59:42 -0300

    Criado arquivo CSS do 'Carousel'

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -1,7 +1,20 @@
 <template>
-
+  <section>
+    <div id="container">
+      <div id="content">
+        <div id="card">
+          <div id="card-content">
+            <div id="image">
+              <img
+                src="https://cdn.oneesports.gg/cdn-data/2022/09/LeagueofLegends_Worlds2022_Logo_Poster.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script lang="ts">
-
-</script>
+<script lang="ts" setup></script>
